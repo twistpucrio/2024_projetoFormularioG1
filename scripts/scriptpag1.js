29c7300c3f68a704a16470335d59d586f3f718cc
Danielle Guimarães Cruz de Oliveira <danielleguimaraes@tecgraf.puc-rio.br>
2024-08-25 21:26:50 -0300

    Resolvendo os conflitos do java script da página busca

--- scripts/scriptpag1.js
+++ scripts/scriptpag1.js
@@ -1,12 +1,43 @@
-function buscarRegex() {
-    const text = document.getElementById('textArea').value;
-    const regexValue = document.querySelector('input[name="regex"]:checked').value;
-    const regex = new RegExp(regexValue, 'g');
-    const matches = text.match(regex);
-    
-    if (matches) {
-        alert(`Padrão ${regexValue} encontrado, quantidade de ocorrências: ${matches.length}`);
-    } else {
-        alert(`Padrão ${regexValue} não encontrado`);
-    }
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const btnSubmeter = document.getElementById('btnSubmeter');
+    const textArea = document.getElementById('textArea');
+    const selecionado = document.querySelectorAll('input[name="verificacao"]');
+
+    btnSubmeter.addEventListener('click', () => {
+        const text = textArea.value;
+
+        let selecionados = '';
+        selecionado.forEach(radio => {
+            if (radio.checked) {
+                selecionados = radio.value;
+                selecionadoId = radio.id;
+            }
+        });
+
+        if (!selecionados) {
+            alert('Por favor, selecione um padrão de busca.');
+            return;
+        }
+
+        const regex = new RegExp(selecionados, 'g');
+        const matches = text.match(regex);
+
+        if (matches) {
+            alert(`Padrão ${selecionadoId} encontrado, quantidade de ocorrências ${matches.length}`);
+        } else {
+            alert(`Padrão ${selecionadoId} não encontrado`);
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const limpar = document.getElementById('btnLimpar');
+    const textBox = document.getElementById('textArea');
+    const radioButtons = document.querySelectorAll('input[type="radio"][name="verificacao"]');
+
+    limpar.addEventListener('click', () => {
+        textBox.value = '';
+
+        radioButtons.forEach(radio => radio.checked = false);
+    });
+});
